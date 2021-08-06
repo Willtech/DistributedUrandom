10eab20110cae2457337cf3b9cd88e32619a243a
Damian Williamson <willtech@live.com.au>
2021-08-06 20:45:30 +1000

    Operational urandomapi.php
    Included cache-control
    
    Signed-off-by: Damian Williamson <willtech@live.com.au>

--- API/urandomapi.php
+++ API/urandomapi.php
@@ -1,6 +1,12 @@
 <?php
 //According to manual for rand_bytes and random_int the getrandom(2) syscall is used and this makes use of /dev/urandom
 
+//Set headers for no-cache
+header('Expires: Sat, 04 Jan 2014 00:00:00 GMT');
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0', FALSE);
+header('Pragma: no-cache');
+
 //function for API call. Return is data there are no descriptors.
 function api ($bytes=512) {
   return base64_encode(random_bytes($bytes));
