9165dbd102ac0207d65c1f3e486d6c125f6b95f2
Damian Williamson <willtech@live.com.au>
2021-08-10 10:53:31 +1000

    Initialise remotes duigcoremote.php duigcoremote.py
    Create file entries in index
    
    Signed-off-by: Damian Williamson <willtech@live.com.au>

---
+++ Remote/PHP/duigcoremote.php
@@ -0,0 +1,22 @@
+<?php
+/* ************************************************ */
+/*                                                  */
+/* Distributed Urandom Increment Global CoOperative */
+/*                                                  */
+/* This file is part of the "DUIGCO API" package    */
+/* Produced for: DUIGCO API                         */
+/*                                                  */
+/* Source Code produced by Willtech 2021            */
+/* v0.1 hand coded by HRjJ                          */
+/*                                                  */
+/* ************************************************ */
+
+//Set headers for no-cache
+header('Expires: Sat, 04 Jan 2014 00:00:00 GMT');
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0', FALSE);
+header('Pragma: no-cache');
+
+//Placeholder
+echo ("Hello world!");
+?>
---
+++ Remote/Python/duigcoremote.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+## Distributed Urandom Increment Global CoOperative
+# DUIGCO REMOTE
+# duigcoremote.py script
+# Source Code produced by Willtech 2021
+# v0.1 hand coded by HRjJ
+
+## placeholder
+print "Hello world!"
